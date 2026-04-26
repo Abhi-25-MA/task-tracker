833abf5ff9e0d550dfb71d74c10e8910e78f927c
Abhinaya <maabhi2505@gmail.com>
2026-04-27 01:27:11 +0530

    add due_date field fix

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,13 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('add/', views.add_task),
-    path('delete/<int:id>/', views.delete_task),
-    path('complete/<int:id>/', views.toggle_complete),
-    path('edit/<int:id>/', views.edit_task),
-
-    path('login/', views.login_view),
-    path('logout/', views.logout_view),
-    path('signup/', views.signup_view),
+    path('', views.home, name='home'),
+    path('add/', views.add_task, name='add_task'),
+    path('delete/<int:id>/', views.delete_task, name='delete_task'),
+    path('complete/<int:id>/', views.toggle_complete, name='toggle_complete'),
+    path('edit/<int:id>/', views.edit_task, name='edit_task'),
+    path('signup/', views.signup_view, name='signup'),
 ]
